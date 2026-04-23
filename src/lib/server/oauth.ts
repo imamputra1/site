2b0ebbabb91917cfc700a11a5456b029f92cd374
imamputra1 <a.i.syahputra415@gmail.com>
2026-04-23 11:06:01 +0700

    fix github error

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -1,11 +1,6 @@
 import { GitHub } from 'arctic';
-// 1. HAPUS ATAU COMMENT BARIS INI:
-// import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, BASE_URL } from '$env/static/private';
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, BASE_URL } from '$env/static/private';
 
-// 2. GUNAKAN DYNAMIC ENV SEBAGAI GANTINYA:
-import { env } from '$env/dynamic/private';
-
-// Jika di bawahnya ada deklarasi variabel, ubah cara panggilnya:
-// export const github = new GitHub(env.GITHUB_CLIENT_ID || '', env.GITHUB_CLIENT_SECRET || '', env.BASE_URL || '');
+export const github = new GitHub(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, BASE_URL);
 
 // TODO: Update redirect URI
